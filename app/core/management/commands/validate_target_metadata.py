552be909bf5bc4f493a06881bd602cbbfa3d67c9
cscholer591 <cscholer@gps.deloitte.com>
2025-10-10 12:29:37 -0400

    adding asterisk for one keyword argument

--- app/core/management/commands/validate_target_metadata.py
+++ app/core/management/commands/validate_target_metadata.py
@@ -69,7 +69,8 @@ def logging_required_recommended(validation_result,
                                  record_status_result,
                                  required_column_list,
                                  recommended_column_list,
-                                 flattened_source_data, 
+                                 flattened_source_data,
+                                 *, 
                                  ind):
     """ Logging required recommended"""
     # validate for required values in data
@@ -139,7 +140,7 @@ def validate_target_using_key(target_data_dict, required_column_list,
                                          required_column_list,
                                          recommended_column_list,
                                          flattened_source_data, 
-                                         ind = index)
+                                         ind=index)
         # Type checking for values in metadata
         for item in flattened_source_data:
 
